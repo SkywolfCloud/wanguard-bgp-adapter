7266ec8640854fb9ab7dbfc0312af5845eaa1e5b
pppwaw <pppwaw@pppwaw.top>
2024-10-07 00:21:15 +0800

    fix bgp community

--- advertise.py
+++ advertise.py
@@ -22,6 +22,13 @@ def get_community_from_gobgp(stub, resource, name, family):
 # 假设你已经有了 gobgp 的 .proto 文件编译生成的 Python 代码：gobgp_pb2 和 gobgp_pb2_grpc
 
 
+def int_to_bgp_community(value):
+    assert 0 <= value < 2**32, "Value must be a 32-bit integer"
+    high = value >> 16
+    low = value & 0xFFFF
+    return f"{high}:{low}"
+
+
 def get_as_path_and_community(prefix):
     # 创建与 GoBGP 的 gRPC 连接
     channel = grpc.insecure_channel(Config.GOBGP_URI)
@@ -50,29 +57,27 @@ def get_as_path_and_community(prefix):
             if attr.Is(attribute_pb2.CommunitiesAttribute.DESCRIPTOR):
                 communities_attr = attribute_pb2.CommunitiesAttribute()
                 attr.Unpack(communities_attr)
-                communities = communities_attr.communities
-            if attr.Is(attribute_pb2.ExtendedCommunitiesAttribute.DESCRIPTOR):
-                extended_communities_attr = attribute_pb2.ExtendedCommunitiesAttribute()
-                attr.Unpack(extended_communities_attr)
-                extended_communities = extended_communities_attr.communities
+                communities = [
+                    int_to_bgp_community(i) for i in communities_attr.communities
+                ]
             if attr.Is(attribute_pb2.LargeCommunitiesAttribute.DESCRIPTOR):
                 large_communities_attr = attribute_pb2.LargeCommunitiesAttribute()
                 attr.Unpack(large_communities_attr)
-                large_communities = large_communities_attr.communities
+                large_communities = [
+                    f"{i.global_admin}:{i.local_data1}:{i.local_data2}"
+                    for i in large_communities_attr.communities
+                ]
 
     channel.close()
     return as_path, communities, extended_communities, large_communities
 
 
-def broadcast_prefix_with_community(
-    prefix, community, extended_community, large_community, as_path
-):
+def broadcast_prefix_with_community(prefix, community, large_community, as_path):
     # 配置 ExaBGP 的命名管道或输入文件的路径
     with open(Config.EXABGP_CMD, "w") as f:
         # 构造 ExaBGP 的命令
         command = (
             f"announce route {prefix} next-hop self community [{' '.join(community)}] "
-            f"extended-community [{' '.join(extended_community)}] "
             f"large-community [{' '.join(large_community)}] "
             f"as-path [{as_path}] "
         )
@@ -86,15 +91,11 @@ def main():
         sys.exit(1)
 
     prefix = sys.argv[1]
-    as_path, community, extended_community, large_community = get_as_path_and_community(
-        prefix
-    )
+    as_path, community, large_community = get_as_path_and_community(prefix)
 
     # 添加 Community
     community.append(Config.EXABGP_COMMUNITY)
-    broadcast_prefix_with_community(
-        prefix, community, extended_community, large_community, as_path
-    )
+    broadcast_prefix_with_community(prefix, community, large_community, as_path)
 
 
 if __name__ == "__main__":
